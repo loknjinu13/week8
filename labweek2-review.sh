3816d287ed2261c627a65b9d10c0229efa43024d
FONGOH LOKNJINU <atloknjinu@gmail.com>
2018-03-10 04:10:25 +0000

    Update of script
    edited line 6 to make 3 directories, line 13 with an absolute path ~/hi.txt, line 16 with Z10=Zidane; export Z10; echo $Z10 >>~/lo.txt, line 17 with $G11>>~/lo.txt. Line 23 with ps ax >>~/hi.txt, line 25 cat ~/hi.txt

--- labweek2-review.sh
+++ labweek2-review.sh
@@ -3,24 +3,24 @@
 title="Week2labreview"
 RIGHT_NOW=$(date +"%x %r %z")
 MY_NAME=$("whoami")
-#let's create 2 directories
+#let's create 3 directories
 mkdir Pastor Hope Secure
 #let's change directories
 cd Pastor
 #Let's create files
 touch hi.txt lo.txt fo.txt
 #let's input your name into a file
-echo $MY_NAME >hi.txt
+echo ${MY_NAME}>~/hi.txt
 # Exporting a variable into a file
-CR7=CristanoRonaldo; export CR7; echo $CR7 >lo.txt
-Z10=Zidane; export z10; echo $z10 >>lo.txt
-G11=GaryBale; export G11; echo $G11>>lo.txt
+CR7=CristanoRonaldo; export CR7; echo $CR7 >~/lo.txt
+Z10=Zidane; export Z10; echo $Z10 >>~/lo.txt
+G11=GaryBale; export G11; echo $G11>>~/lo.txt
 #copy lo.txt to fo.txt
 cp lo.txt fo.txt
 #check the differences in files
 diff lo.txt hi.txt
 #let's know the running processes
-ps ax  > hi.txt
+ps ax >>~/hi.txt
 #let's view hi.txt
-cat hi.txt 
+cat ~/hi.txt 
 #end script
